9ec46278a439c311b3fe3dc01da323d9c0a140ef
drako <noam98038@gmail.com>
2025-09-26 12:59:27 +0300

    Add project files.

---
+++ LLStudy_Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLStudy_Models
+{
+    public class Book
+    {
+        string book_ID;
+        string author_name;
+        double book_price;
+        string book_name;
+        string in_stock;
+        string subjectID;
+        string pdf_url_book;
+
+        public string Book_ID {  get { return book_ID; } set {  book_ID = value; } }
+        [Required(ErrorMessage = "Name was not set")]
+        [StringLength(20,MinimumLength = 2, ErrorMessage ="Lenght is not between 2 and 20 characters.")]
+        [FirstLetterCapital(ErrorMessage = "First letter isn't capital.")]
+        public string Author_name { get { return author_name; } set { author_name = value; } }
+        public string Book_name { get { return book_name; } set { book_name = value; } }
+        public string In_stock { get { return in_stock; } set { in_stock = value; } }
+        public string SubjectID { get { return subjectID; } set { subjectID = value; } }
+        public double Book_price { get { return book_price; } set { book_price = value; } }
+        public string Pdf_url_book { get { return pdf_url_book; } set { pdf_url_book = value; } }
+
+
+    }
+}
---
+++ LLStudy_Models/CatalogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLStudy_Models
+{
+    public class CatalogViewModel
+    {
+        public List<Book> Books { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Registered> registereds { get; set; }
+    }
+}
