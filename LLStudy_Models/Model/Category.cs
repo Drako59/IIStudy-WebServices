2fefcebaf29b4d399226d1dac3abf6bebbfed42b
ayele <noam98038@gmail.com>
2025-10-16 22:11:47 +0300

    add model

---
+++ LLStudy_Models/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLStudy_Models.Model
+{
+    public class CategoryModel
+    {
+        string subjectID;
+        string subject_name;
+        public string SubjectID { get { return subjectID; } set { subjectID = value; } }
+        public string Subject_name { get { return subject_name; } set { subject_name = value; } }
+    }
+}
