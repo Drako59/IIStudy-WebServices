51f68d7ad4547aed2b33beb15c101136dbccec8d
ayele <noam98038@gmail.com>
2026-02-06 00:07:08 +0200

    hhh

--- LLstudyAPI/ORM/Repositorys/RegisteredRepository.cs
+++ LLstudyAPI/ORM/Repositorys/RegisteredRepository.cs
@@ -98,6 +98,9 @@ namespace LLstudyWS.ORM
             if (file == null || file.Length == 0)
                 throw new Exception("Empty file");
 
+            //Registered reg2 = this.GetByID(registeredID);
+            //File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images", "RegisteredImages",reg2.ImagePath));
+
             //string path = Path.Combine(Directory.GetCurrentDirectory()!, "App_Data","RegisteredsImages");
             string path = Path.Combine(Directory.GetCurrentDirectory()!, "wwwroot", "Images", "RegisteredImages");
 
