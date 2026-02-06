b32d222b32e57cab2f6863f53f21ff1aa40c7712
USER <USER@DESKTOP-RD0CB86>
2026-02-06 11:33:28 +0200

    hii

--- IIStudyDESKTOP/MainWindow.xaml.cs
+++ IIStudyDESKTOP/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace IIStudyDESKTOP
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
